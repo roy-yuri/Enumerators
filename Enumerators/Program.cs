9d0ab7203906b41c1ef2419267e72e5db2adba31
Yuriy <Yuriy@DESKTOP-ETJK50N>
2021-11-22 00:22:48 +0200

    Add project files.

---
+++ Enumerators/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections;
+
+Console.WriteLine("Hello, World!");
+
+MyRange a = new MyRange(2, 5);
+
+
+public class MyRange //: IEnumerable 
+{
+    private int[] Numbers;
+
+    public MyRange(int min, int max)
+    {
+        // Constructor
+        Numbers = new int[max-min];
+        for (int i = 0; i < max-min; i++)
+        {
+            Numbers[i] = min+i;
+        }
+    }
+
+
+}
