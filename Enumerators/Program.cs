4bd9d4bf68be8197227db5b4385fdc0dabf3e8ee
roy-yuri <roy.yuri@gmail.com>
2021-11-22 00:45:05 +0200

    Update Program.cs

--- Enumerators/Program.cs
+++ Enumerators/Program.cs
@@ -1,24 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 using System;
 using System.Collections;
+using System.Linq;
 
 Console.WriteLine("Hello, World!");
 
-MyRange a = new MyRange(2, 5);
+for (var e = new MyRange(12, 31); e.MoveNext();)
+{
+    Console.WriteLine(e.Current);
+}
 
 
-public class MyRange //: IEnumerable 
+public class MyRange : IEnumerator 
 {
     private int[] Numbers;
+    int position = -1;
 
     public MyRange(int min, int max)
     {
         // Constructor
-        Numbers = new int[max-min];
-        for (int i = 0; i < max-min; i++)
+        // +1 because index starts from 0
+        Numbers = new int[max-min+1];
+        for (int i = 0; i < max-min+1; i++)
         {
             Numbers[i] = min+i;
         }
+    }
+
+    public object Current
+    {
+        get
+        {
+            if (position == -1 || position >= Numbers.Length)
+                throw new InvalidOperationException();
+            return Numbers[position];
+        }
+    }
+
+    public bool MoveNext()
+    {
+        if (position < Numbers.Length - 1)
+        {
+            position++;
+            return true;
+        }
+        else
+            return false;
+    }
+        public void Reset()
+    {
+        position = -1;
     }
 
 
